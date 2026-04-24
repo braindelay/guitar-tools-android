243b42b99dae653d4e9aed6dc7c6fd5badcd4d3e
Daniel Bray <daniel@braindelay.com>
2026-04-24 14:46:49 +0100

    v16

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,7 +22,7 @@ android {
         applicationId = "com.braindelay.guitartools"
         minSdk = 24
         targetSdk = 36
-        versionCode = 15
+        versionCode = 16
         versionName = "1.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
